9c4ff608285c85f05d8d8fcd6b25d07bf7dbf4bd
m.kuzhnenkov <m.kuzhnenkov@gortis.ru>
2022-11-29 21:13:52 +0400

    Camera has started work. Future plans: Taking a picture, zoom by tap

--- app/src/main/java/com/mikeworkflow/mcga/MainActivity.kt
+++ app/src/main/java/com/mikeworkflow/mcga/MainActivity.kt
@@ -1,58 +1,158 @@
 package com.mikeworkflow.mcga
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Camera
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.DisplayMetrics
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import androidx.core.content.ContextCompat
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import com.mikeworkflow.mcga.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PermissionListener {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mCamera: Camera
+    private lateinit var mCameraLayout: FrameLayout
+    private lateinit var mCameraPreview: CameraPreview
+
+    override fun onPermissionGranted() {
+        initCameraPreview()
+    }
+
+    override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
+        mCameraLayout = binding.cameraLayout
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        if (ContextCompat.checkSelfPermission(
+                this,
+                CAMERA_SERVICE
+            ) == PackageManager.PERMISSION_DENIED
+        )
+            TedPermission.with(this)
+                .setPermissionListener(this)
+                .setPermissions(Manifest.permission.CAMERA)
+                .check()
+        else
+            initCameraPreview()
+    }
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+    private fun initCameraPreview() {
+        try {
+            mCamera = getCameraInstance()
+        } catch (e: Exception) {
         }
+
+        mCameraPreview = CameraPreview(this, mCamera, this)
+        mCameraLayout.addView(mCameraPreview)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    private fun getCameraInstance(): Camera {
+        try {
+            return Camera.open()
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
+        return Camera.open()
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    public fun setupCameraLayout(width: Int, height: Int) {
+        var params = mCameraLayout.layoutParams
+        params.height = height
+        params.width = width
+        mCameraLayout.layoutParams = params
+    }
+
+    class CameraPreview(context: Context, camera: Camera, mainActivity: MainActivity) : SurfaceView(context),
+        SurfaceHolder.Callback {
+        private var mCamera: Camera = camera
+        private var mHolder = holder
+        private var mSupportedPreviewSizes = camera.parameters.supportedPreviewSizes
+        private lateinit var mPreviewSize: Camera.Size
+        private val mainActivity = mainActivity
+
+        init {
+            mHolder.addCallback(this)
+            mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
+        }
+
+
+        override fun surfaceCreated(holder: SurfaceHolder) {
+            try {
+                mCamera.setPreviewDisplay(holder)
+                mCamera.startPreview()
+            } catch (e: Exception) {
+            }
+        }
+
+        override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+            if (mHolder.surface == null)
+                return
+            try {
+                mCamera.stopPreview()
+            } catch (e: Exception) {
+            }
+
+            try {
+                mCamera.setPreviewCallback(object : Camera.PreviewCallback {
+                    override fun onPreviewFrame(data: ByteArray?, camera: Camera?) {
+
+                    }
+                })
+                mCamera.parameters.focusMode = Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO
+                mCamera.parameters.previewSize.height = mPreviewSize.height
+                mCamera.parameters.previewSize.width = mPreviewSize.width
+                mCamera.parameters.zoom = 0
+                mCamera.setPreviewDisplay(mHolder)
+                mainActivity.setupCameraLayout(mPreviewSize.width, mPreviewSize.height)
+                mCamera.startPreview()
+
+            } catch (e: Exception) {
+            }
+        }
+
+        override fun surfaceDestroyed(holder: SurfaceHolder) {}
+
+        override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+            val dm : DisplayMetrics = resources.displayMetrics
+            val width = dm.widthPixels.toDouble()
+            val height = dm.heightPixels.toDouble()
+
+            if (mSupportedPreviewSizes == null) return
+
+            var optimalSize: Camera.Size? = null
+            var deltaH : Double = Double.MAX_VALUE
+            var deltaW: Double = Double.MAX_VALUE
+            for (size in mSupportedPreviewSizes) {
+                if (width - size.width < deltaW && height - size.height < deltaH) {
+                    optimalSize = size
+                    deltaW = width - size.width
+                    deltaH = height - size.height
+
+                }
+            }
+            mPreviewSize = optimalSize!!
+            if (deltaH>deltaW) {
+                mPreviewSize.height = height.toInt()
+                mPreviewSize.width = (width * height/mPreviewSize.height).toInt()
+            } else {
+                mPreviewSize.height = (height * width / mPreviewSize.width).toInt()
+                mPreviewSize.width = width.toInt()
+            }
+
+            setMeasuredDimension(mPreviewSize.width, mPreviewSize.height)
+
+        }
     }
 }
